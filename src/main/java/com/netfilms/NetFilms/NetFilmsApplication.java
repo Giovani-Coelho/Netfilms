59254e12f645c46d9758230a576e05816521f52c
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2023-09-04 16:57:39 -0300

    first

---
+++ src/main/java/com/netfilms/NetFilms/NetFilmsApplication.java
@@ -0,0 +1,12 @@
+package com.netfilms.NetFilms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetFilmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetFilmsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/netfilms/NetFilms/NetFilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netfilms.NetFilms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetFilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
